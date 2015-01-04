64d471a41994af67a1d1e1cb364906c3af43d24b
format <fangjian0423@gmail.com>
2015-01-04 13:05:48 +0800

    modify the construct of sql, using mybatis's SQL class

--- src/main/java/org/format/mybatis/helper/provider/SqlProvider.java
+++ src/main/java/org/format/mybatis/helper/provider/SqlProvider.java
@@ -1,8 +1,7 @@
 package org.format.mybatis.helper.provider;
 
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.jdbc.SQL;
 import org.format.mybatis.helper.annotation.Column;
-import org.format.mybatis.helper.entity.Entity;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
@@ -11,49 +10,49 @@ import java.util.Map;
 public class SqlProvider {
 
     public String query(Map<String, Object> dataMap) {
-        Class entity = getEntity(dataMap);
-        String sql = select(entity.getSimpleName().toUpperCase(), "*");
-        sql += where(null);
-        return sql;
+        final Class entity = getEntity(dataMap);
+        return new SQL() {
+            {
+                SELECT("*");
+                FROM(entity.getSimpleName().toUpperCase());
+                //TODO 加上查询条件
+            }
+        }.toString();
     }
 
     public String count(Map<String, Object> dataMap) {
-        Class entity = getEntity(dataMap);
-        String sql = select(entity.getSimpleName().toUpperCase(), "count(*)");
-        sql += where(null);
-        return sql;
+        final Class entity = getEntity(dataMap);
+        return new SQL() {
+            {
+                SELECT("count(*)");
+                FROM(entity.getSimpleName().toUpperCase());
+                //TODO 加上查询条件
+            }
+        }.toString();
     }
 
     public String getAll(Map<String, Object> dataMap) {
-        Class entity = getEntity(dataMap);
-        String sql = select(entity.getSimpleName().toUpperCase(), "count(*)");
-        return sql;
+        final Class entity = getEntity(dataMap);
+        return new SQL() {
+            {
+                SELECT("*");
+                FROM(entity.getSimpleName().toUpperCase());
+            }
+        }.toString();
     }
 
     public String insert(Map<String, Object> dataMap) {
         try {
-            Class entity = getEntity(dataMap);
-            Field[] fields = entity.getDeclaredFields();
-            StringBuilder sql = new StringBuilder("insert into " + entity.getSimpleName().toUpperCase() + "(");
-            for(int i = 0; i < fields.length; i ++) {
-                if(fields[i].getAnnotation(Column.class) != null) {
-                    sql.append(fields[i].getAnnotation(Column.class).value());
-                } else {
-                    sql.append(fields[i].getName());
+            final Class entity = getEntity(dataMap);
+            final Field[] fields = entity.getDeclaredFields();
+            return new SQL() {
+                {
+                    INSERT_INTO(entity.getSimpleName().toUpperCase());
+                    for(int i = 0; i < fields.length; i ++) {
+                        VALUES(fields[i].getAnnotation(Column.class) == null ? fields[i].getName() : fields[i].getAnnotation(Column.class).value(), "#{model." + fields[i].getName() + "}");
+                    }
                 }
-                if(i != fields.length - 1) {
-                    sql.append(",");
-                }
-            }
-            sql.append(") values(");
-            for(int i = 0; i < fields.length; i ++) {
-                sql.append("#{model." + fields[i].getName() + "}");
-                if(i != fields.length - 1) {
-                    sql.append(",");
-                }
-            }
-            sql.append(")");
-            return sql.toString();
+            }.toString();
         } catch(Exception e) {
             e.printStackTrace();
         }
@@ -61,11 +60,11 @@ public class SqlProvider {
     }
 
     public String update(Map<String, Object> dataMap) {
-        Class entity = getEntity(dataMap);
         try {
+            final Class entity = getEntity(dataMap);
             Field[] fields = entity.getDeclaredFields();
             Object model = dataMap.get("model");
-            Map<String, Object> entityData = new HashMap<String, Object>();
+            final Map<String, Object> entityData = new HashMap<String, Object>();
             for(int i = 0; i < fields.length; i ++) {
                 fields[i].setAccessible(true);
                 if(fields[i].get(model) != null) {
@@ -73,11 +72,15 @@ public class SqlProvider {
                     entityData.put(column, fields[i].getName());
                 }
             }
-            StringBuilder sql = new StringBuilder("update " + entity.getSimpleName().toUpperCase() + " set ");
-            for(String column : entityData.keySet()) {
-                sql.append(column + "=#{model." + entityData.get(column) + "},");
-            }
-            return sql.substring(0, sql.length() - 1) + " where id = #{model.id}";
+            return new SQL() {
+                {
+                    UPDATE(entity.getSimpleName().toUpperCase());
+                    for(String column : entityData.keySet()) {
+                        SET(column + "=#{model." + entityData.get(column) + "}");
+                    }
+                    WHERE("id = #{model.id}");
+                }
+            }.toString();
         } catch(Exception e) {
             e.printStackTrace();
         }
@@ -85,21 +88,13 @@ public class SqlProvider {
     }
 
     public String delete(Map<String, Object> dataMap) {
-        Class entity = getEntity(dataMap);
-        String sql = "delete from " + entity.getSimpleName().toUpperCase() + " where id = #{id}";
-        return sql;
-    }
-
-    private String select(String tableName, String fields) {
-        return "select " + fields + " from " + tableName;
-    }
-
-    private String where(Map<String, Object> params) {
-        if(params == null || params.size() == 0) {
-            return "";
-        }
-        StringBuilder sb = new StringBuilder();
-        return sb.toString();
+        final Class entity = getEntity(dataMap);
+        return new SQL() {
+            {
+                DELETE_FROM(entity.getSimpleName().toUpperCase());
+                WHERE("id = #{id}");
+            }
+        }.toString();
     }
 
     private Class getEntity(Map<String, Object> dataMap) {
