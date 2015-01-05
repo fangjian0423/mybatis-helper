957bcea3e0d6e02b8523434eafdc2b0351ab8ae7
format <fangjian0423@gmail.com>
2015-01-05 22:27:17 +0800

    remove test code

--- src/main/java/org/format/mybatis/helper/dao/StudentDao.java
+++
@@ -1,8 +0,0 @@
-package org.format.mybatis.helper.dao;
-
-import org.format.mybatis.helper.entity.Student;
-import org.format.mybatis.helper.entity.StudentDto;
-
-public interface StudentDao extends BaseDao<Student, StudentDto> {
-
-}
